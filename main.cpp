7d72c5facbea8193f22ed228ad953fde3a1be487
LamaTarek <75127993+LamaTarek@users.noreply.github.com>
2021-08-05 17:14:48 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<string>
+#include<iomanip>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<numeric>
+#include<cstring>
+typedef long long ll;
+using namespace std;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+}
+int main()
+{
+    init();
+    //freopen("max-pair.in","r",stdin);
+    int t,n;
+    cin>>t;
+    while (t--){
+        cin>>n;
+        cout<<1<<" "<<n-1<<"\n";
+    }
+    return 0;
+}
