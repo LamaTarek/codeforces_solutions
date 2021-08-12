2bd6ed0afe2fb1b0843bf4e5b8461dc56ce77fac
LamaTarek <88500194+LamaTarek@users.noreply.github.com>
2021-08-12 04:43:56 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<string>
+#include<iomanip>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<numeric>
+#include<cstring>
+#include <set>
+#include <stack>
+#include <deque>
+#include <queue>
+typedef long long ll;
+using namespace std;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+}
+#define posmod(v,m) ((v)%(m)+(m))%(m)
+#define cl(x,val) memset(x,val,sizeof(x))
+deque<int>q;
+int main() {
+    init();
+    //freopen("file.in","r",stdin);
+    int n;
+    cin >> n;
+    int wires[n + 1];
+    wires[0]=0;
+    for (int i = 1; i <= n; i++)cin >> wires[i];
+    int q, index, bird;
+    cin >> q;
+    while (q--) {
+        cin >> index >> bird;
+        if (index != 1)wires[index - 1] += (bird - 1);
+        if (index != n)wires[index + 1] += (wires[index] - bird);
+        wires[index] = 0;
+    }
+    for (int i = 1; i <= n; ++i) {
+        cout << wires[i] << "\n";
+    }
+    return 0;
+}
