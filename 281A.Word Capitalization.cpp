e127002c8431b558b47df352386d18b2a93968b2
Lama Tarek <88500194+LamaTarek@users.noreply.github.com>
2022-08-02 17:03:25 +0200

    Create 281A.Word Capitalization.cpp
    problem link : https://codeforces.com/problemset/problem/281/A

---
+++ 281A.Word Capitalization.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+void init() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+}
+
+int main() {
+    init();
+    string s;
+    cin>>s;
+    s[0]= toupper(s[0]);
+    cout<<s<<"\n";
+    return 0;
+}
